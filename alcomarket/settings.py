2129fb3e10e043221420a6fb435b5f86e64c315c
Vitaliy Pavlov <v-2841@yandex.ru>
2023-10-23 16:05:42 +0400

    for prod

--- alcomarket/settings.py
+++ alcomarket/settings.py
@@ -76,13 +76,6 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
@@ -108,9 +101,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = '/var/www/alcomarket/static/'
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/var/www/alcomarket/media/'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
