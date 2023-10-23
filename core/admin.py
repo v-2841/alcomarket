01629f5c434264b280ce404451d4fedcd34ddc35
Vitaliy Pavlov <v-2841@yandex.ru>
2023-10-23 17:13:37 +0400

    mail

--- core/admin.py
+++ core/admin.py
@@ -4,3 +4,4 @@ from django.contrib import admin
 admin.site.site_title = 'Alcomarket'
 admin.site.site_header = 'Alcomarket'
 admin.site.index_title = 'Администрирование сайта'
+admin.site.site_url = 'alcomarket.ru'
